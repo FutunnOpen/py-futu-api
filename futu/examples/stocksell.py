8753b6628c82a82bce76363ef7970b4619b4723d
alexqli <alexqli@futunn.com>
2020-04-03 19:33:10 +0800

    完善例子的注释，代码结构

--- futu/examples/stocksell.py
+++ futu/examples/stocksell.py
@@ -4,13 +4,13 @@
 """
 from time import sleep
 import futu as ft
-
+import sys
 
 def simple_sell(quote_ctx, trade_ctx, stock_code, trade_price, volume, trade_env, order_type=ft.OrderType.NORMAL):
-    """简单卖出函数"""
-    lot_size = 0
+    """简单卖出函数。取到股票每手的股数后，就下单卖出"""
+    lot_size = 0  # 每手多少股
     while True:
-        sleep(0.1)
+        sleep(1)
         if lot_size == 0:
             ret, data = quote_ctx.get_market_snapshot(stock_code)
             lot_size = data.iloc[0]['lot_size'] if ret == ft.RET_OK else 0
@@ -20,7 +20,7 @@ def simple_sell(quote_ctx, trade_ctx, stock_code, trade_price, volume, trade_env
             elif lot_size <= 0:
                 raise Exception('lot size error {}:{}'.format(lot_size, stock_code))
 
-        qty = int(volume // lot_size) * lot_size
+        qty = int(volume // lot_size) * lot_size  # 将数量调整为整手的股数
         ret, data = trade_ctx.place_order(price=trade_price, qty=qty, code=stock_code,
                                           trd_side=ft.TrdSide.SELL, trd_env=trade_env, order_type=order_type)
         if ret != ft.RET_OK:
@@ -32,8 +32,8 @@ def simple_sell(quote_ctx, trade_ctx, stock_code, trade_price, volume, trade_env
 
 
 def smart_sell(quote_ctx, trade_ctx, stock_code, volume, trade_env, order_type=ft.OrderType.NORMAL):
-    """智能卖出函数"""
-    lot_size = 0
+    """智能卖出函数。取到股票每手的股数，以及摆盘数据后，就以买一价下单卖出"""
+    lot_size = 0  # 每手多少股
     while True:
         if lot_size == 0:
             ret, data = quote_ctx.get_market_snapshot(stock_code)
@@ -44,15 +44,16 @@ def smart_sell(quote_ctx, trade_ctx, stock_code, volume, trade_env, order_type=f
             elif lot_size <= 0:
                 raise Exception('lot size error {}:{}'.format(lot_size, stock_code))
 
-        qty = int(volume / lot_size) * lot_size
-        ret, data = quote_ctx.get_order_book(stock_code)
+        qty = int(volume / lot_size) * lot_size  # 将数量调整为整手的股数
+        ret, data = quote_ctx.get_order_book(stock_code)  # 获取摆盘
         if ret != ft.RET_OK:
             print("can't get orderbook, retrying:{}".format(data))
             continue
 
-        price = data['Bid'][0][0]
+        price = data['Bid'][0][0]  # 取得买一价
         print('smart_sell bid price is {}'.format(price))
 
+        # 以买一价下单卖出
         ret, data = trade_ctx.place_order(price=price, qty=qty, code=stock_code,
                                           trd_side=ft.TrdSide.SELL, trd_env=trade_env, order_type=order_type)
         if ret != ft.RET_OK:
@@ -65,23 +66,24 @@ def smart_sell(quote_ctx, trade_ctx, stock_code, volume, trade_env, order_type=f
 
 
 if __name__ =="__main__":
-
     ip = '127.0.0.1'
     port = 11111
-
-    code = 'HK.00700'
-    unlock_pwd = '123456'
-    trd_env = ft.TrdEnv.SIMULATE
-    order_type = ft.OrderType.NORMAL
+    code = 'HK.00700'      # 要卖出的股票
+    unlock_pwd = '123456'  # 解锁交易密码
+    trd_env = ft.TrdEnv.SIMULATE  # 模拟交易
+    order_type = ft.OrderType.NORMAL  # 订单类型
 
     quote_ctx = ft.OpenQuoteContext(ip, port)
     trd_ctx = ft.OpenHKTradeContext(ip, port)
 
-    quote_ctx.subscribe(code, ft.SubType.ORDER_BOOK)
-    print("* unlock_trade:{}".format(trd_ctx.unlock_trade(unlock_pwd)))
-
-    simple_sell(quote_ctx, trd_ctx, code, 280.0, 100, trd_env, order_type)
-    smart_sell(quote_ctx, trd_ctx, code, 100, trd_env, order_type)
+    quote_ctx.subscribe(code, ft.SubType.ORDER_BOOK)  # 订阅摆盘，这样后面才能调用get_order_book
+    ret, data = trd_ctx.unlock_trade(unlock_pwd)
+    if ret == ft.RET_OK:
+        print("* unlock_trade success")
+        simple_sell(quote_ctx, trd_ctx, code, 280.0, 100, trd_env, order_type)
+        smart_sell(quote_ctx, trd_ctx, code, 100, trd_env, order_type)
+    else:
+        print("* unlock_trade fail: ", data)
 
     quote_ctx.close()
     trd_ctx.close()
